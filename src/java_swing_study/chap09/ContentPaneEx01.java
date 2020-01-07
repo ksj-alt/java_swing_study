9e93e59a90f414c2daba728848da74ac48ca056a
ksj-alt <ham_a127@naver.com>
2020-01-07 16:26:53 -0500

    event 핸들러 추가

--- src/java_swing_study/chap09/ContentPaneEx01.java
+++ src/java_swing_study/chap09/ContentPaneEx01.java
@@ -4,12 +4,22 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ContentPaneEx01 extends JFrame {
+import javax.swing.JButton;
+
+public class ContentPaneEx01 extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
+	private JButton btnOK;
+	private JButton btnCancel;
+	private JButton btnIgnore;
 
 
 
@@ -17,12 +27,54 @@ public class ContentPaneEx01 extends JFrame {
 	 * Create the frame.
 	 */
 	public ContentPaneEx01() {
+		initialize();
+	}
+	private void initialize() {
+		setTitle("Contentpane과 JFrame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);	//100, 100 위치에 넓이 450과 높이 300 박스를 만들어라
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));	//margin 안의 박스
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setBackground(Color.ORANGE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		btnOK = new JButton("OK");
+		btnOK.addActionListener(this);
+		contentPane.add(btnOK);
+		
+		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(this);
 
+		contentPane.add(btnCancel);
+		
+		btnIgnore = new JButton("Ignore");
+		btnIgnore.addActionListener(this);
+		contentPane.add(btnIgnore);
+	}
+	
+
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnIgnore) {
+			btnIgnoreActionPerformed(e);
+		}
+		if (e.getSource() == btnCancel) {
+			btnCancelActionPerformed(e);
+		}
+		if (e.getSource() == btnOK) {
+			btnOKActionPerformed(e); 
+		}
+	}
+	
+	protected void btnOKActionPerformed(ActionEvent e) {
+	JOptionPane.showMessageDialog(null, e.getActionCommand()); 
+	}
+	
+	protected void btnCancelActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
+	protected void btnIgnoreActionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(null, e.getActionCommand());
+	}
 }
