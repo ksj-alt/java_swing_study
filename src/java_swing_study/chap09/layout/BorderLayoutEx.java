c78cf9191bee8b20037326aecbd1c5d6f2cd1c1e
ksj-alt <ham_a127@naver.com>
2020-01-07 17:30:12 -0500

    BorderLayout 배치관리자

--- src/java_swing_study/chap09/layout/BorderLayoutEx.java
+++ src/java_swing_study/chap09/layout/BorderLayoutEx.java
@@ -1,48 +1,48 @@
 package java_swing_study.chap09.layout;
 
-import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 @SuppressWarnings("serial")
-public class FlowLayoutEx extends JFrame {
+public class BorderLayoutEx extends JFrame {
 
 	private JPanel contentPane;
 	private JButton btnadd;
-	private JButton btnsub;
-	private JButton btnmul;
 	private JButton btndiv;
 	private JButton btnCalculate;
+	private JButton btnmul;
+	private JButton btnsub;
 
-	public FlowLayoutEx() {
+	public BorderLayoutEx() {
 		initialize();
 	}
 	private void initialize() {
-		setTitle("Flowlayout Sample");
+		setTitle("BorderLayout Sample");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 300, 200);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(30, 20));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.LEFT, 30, 40));
 		
 		btnadd = new JButton("add");
-		contentPane.add(btnadd);
-		
-		btnsub = new JButton("sub");
-		contentPane.add(btnsub);
-		
-		btnmul = new JButton("mul");
-		contentPane.add(btnmul);
+		contentPane.add(btnadd, BorderLayout.NORTH);
 		
 		btndiv = new JButton("div");
-		contentPane.add(btndiv);
+		contentPane.add(btndiv, BorderLayout.WEST);
 		
 		btnCalculate = new JButton("Calculate");
-		contentPane.add(btnCalculate);
+		contentPane.add(btnCalculate, BorderLayout.CENTER);
+		
+		btnmul = new JButton("mul");
+		contentPane.add(btnmul, BorderLayout.EAST);
+		
+		btnsub = new JButton("sub");
+		contentPane.add(btnsub, BorderLayout.SOUTH);
 	}
 
 }
